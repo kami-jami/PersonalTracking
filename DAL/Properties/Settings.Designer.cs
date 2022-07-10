0dabcefb0a3aeede69a32ec93568f9e5c59e267c
a.jami <kami.jami@gmail.com>
2022-07-10 17:23:08 +0430

    Switch Project to visual studio 2019 and config connection string

--- DAL/Properties/Settings.Designer.cs
+++ DAL/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace DAL.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,7 +26,7 @@ namespace DAL.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=PEYGIRLAPTOP\\MSSQLSERVER2017;Initial Catalog=PTdb;Integrated Security" +
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SQLSERVER2019;Initial Catalog=PTdb;Integrated Security" +
             "=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False")]
         public string PTdbConnectionString {
             get {
